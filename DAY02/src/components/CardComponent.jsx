6a26a52f66de1161626727d99b8982a749233d0a
prajjal <prajjaldhar41@gmail.com>
2024-12-18 11:45:20 +0530

    day03 initiated

--- DAY02/src/components/CardComponent.jsx
+++ DAY02/src/components/CardComponent.jsx
@@ -4,7 +4,7 @@ const CardComponent = ({ contacts }) => {
   return (
     <>
       {contacts.map((product) => (
-        <div className="w-[21rem] h-[32rem] bg-gradient-to-r from-indigo-600 to-indigo-900 m-10 rounded-2xl">
+        <div className="w-[21rem] h-[32rem] bg-gradient-to-r from-indigo-600 to-indigo-900 rounded-2xl">
           <h1 className="text-center text-white text-2xl p-6">
             Product Listing
           </h1>
