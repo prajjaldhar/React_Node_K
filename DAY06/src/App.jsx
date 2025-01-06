54196ddd1de0fdbae99426c774f2b3273230ff48
prajjal <prajjaldhar41@gmail.com>
2025-01-06 12:16:57 +0530

    Day 07 button click updated feature

--- DAY06/src/App.jsx
+++ DAY06/src/App.jsx
@@ -1,307 +1,104 @@
 import React, { useState } from "react";
+import products from "./utility/data";
 
 const App = () => {
-  const [count, setcount] = useState(0);
-  // let count = 100;
-  const handleClickIncrement = () => {
-    if (count >= 5) {
-      alert("You can't increment more than 5 times");
-      setcount(5);
-    } else setcount(count + 1);
-    console.log(count);
+  // const [counts, setCounts] = useState(Array(products.length).fill(0));
+
+  // const handleIncrement = (index) => {
+  //   setCounts((prevCounts) =>
+  //     prevCounts.map((count, i) =>
+  //       i === index ? (count >= 5 ? 5 : count + 1) : count
+  //     )
+  //   );
+  // };
+
+  // const handleDecrement = (index) => {
+  //   setCounts((prevCounts) =>
+  //     prevCounts.map((count, i) =>
+  //       i === index ? Math.max(0, count - 1) : count
+  //     )
+  //   );
+  // };
+
+  const [counts, setCount] = useState(Array(products.length).fill(0));
+  const handleIncrement = (index) => {
+    console.log(index);
+    console.log(counts[index]);
+    setCount((prevCounts) =>
+      prevCounts.map((count, i) =>
+        i === index ? (count >= 5 ? 5 : count + 1) : count
+      )
+    );
   };
-  const handleClickDecrement = () => {
-    if (count === 0) alert("No Items to remove");
-    count > 0 ? setcount(count - 1) : 0;
+  const handleDecrement = (index) => {
+    setCount((prevCounts) =>
+      prevCounts.map((count, i) =>
+        i === index ? (count <= 0 ? 0 : count - 1) : count
+      )
+    );
   };
+
   return (
     <div>
       <div className="text-center p-10">
         <h1 className="font-bold text-4xl mb-4">
-          Responsive Product card grid
+          Responsive Product Card Grid
         </h1>
         <h1 className="text-3xl">Tailwind CSS</h1>
       </div>
-      {/* ✅ Grid Section - Starts Here 👇 */}
-      <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-        {/*   ✅ Product card 1 - Starts Here 👇 */}
-        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                Brand
-              </span>
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Product Name
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $149
+      <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center gap-y-20 gap-x-14 mt-10 mb-5">
+        {products.map((product, index) => (
+          <div
+            key={product.id}
+            className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+          >
+            <a href="#">
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                className="h-80 w-72 object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-72">
+                <span className="text-gray-400 mr-3 uppercase text-xs">
+                  {product.brand}
+                </span>
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                  {product.name}
                 </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={20}
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black my-3">
+                    ${product.price}
+                  </p>
+                  <del>
+                    <p className="text-sm text-gray-600 ml-2">
+                      ${product.originalPrice}
+                    </p>
+                  </del>
                 </div>
-              </div>
-              <div className="gap-2 flex ">
-                <button
-                  className="font-bold text-xl"
-                  onClick={handleClickIncrement}
-                >
-                  +
-                </button>
-                <span className="top-3 relative">{count}</span>
-                <button
-                  className="font-bold text-xl"
-                  onClick={handleClickDecrement}
-                >
-                  -
-                </button>
-                <div>
-                  <button className="bg-orange-500 text-white rounded-full p-3 w-32 relative left-20">
+                <div className="gap-2 flex items-center">
+                  <button
+                    className="font-bold text-xl"
+                    onClick={() => handleIncrement(index)}
+                  >
+                    +
+                  </button>
+                  <span className="relative">{counts[index]}</span>
+                  <button
+                    className="font-bold text-xl"
+                    onClick={() => handleDecrement(index)}
+                  >
+                    -
+                  </button>
+                </div>
+                <div className="mt-4">
+                  <button className="bg-orange-500 text-white rounded-full p-3 w-full">
                     Add to Cart
                   </button>
                 </div>
               </div>
-            </div>
-          </a>
-        </div>
-        {/*   🛑 Product card 1 - Ends Here  */}
-        {/*   ✅ Product card 2 - Starts Here 👇 */}
-        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                Brand
-              </span>
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Product Name
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $149
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={20}
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        {/*   🛑 Product card 2- Ends Here  */}
-        {/*   ✅ Product card 3 - Starts Here 👇 */}
-        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                Brand
-              </span>
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Product Name
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $149
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={20}
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        {/*   🛑 Product card 3 - Ends Here  */}
-        {/*   ✅ Product card 4 - Starts Here 👇 */}
-        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1651950540805-b7c71869e689?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                Brand
-              </span>
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Product Name
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $149
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={20}
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        {/*   🛑 Product card 4 - Ends Here  */}
-        {/*   ✅ Product card 5 - Starts Here 👇 */}
-        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1649261191624-ca9f79ca3fc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                Brand
-              </span>
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Product Name
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $149
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={20}
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        {/*   🛑 Product card 5 - Ends Here  */}
-        {/*   ✅ Product card 6 - Starts Here 👇 */}
-        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1649261191606-cb2496e97eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                Brand
-              </span>
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Product Name
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $149
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={20}
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        {/*   🛑 Product card 6 - Ends Here  */}
+            </a>
+          </div>
+        ))}
       </section>
     </div>
   );
